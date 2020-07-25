848f9f275d4ac93f516f106b37d263316f4cd57f
Antonio Barandica <anjose.121@gmail.com>
2020-07-25 11:03:54 -0500

    Add blog in Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,13 +13,16 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div id="navMenu">
             <div className="navbar-start has-text-centered">
-              <a className="navbar-item" href="#home">
+              <a className="navbar-item" href="/#home">
                 Home
               </a>
-              <a className="navbar-item" href="#about">
+              <a className="navbar-item" href="/#about">
                 Acerca de mi
               </a>
-              <a className="navbar-item" href="#contact">
+              <a className="navbar-item" href="/blog">
+                Blog
+              </a>
+              <a className="navbar-item" href="/#contact">
                 Contacto
               </a>
             </div>
